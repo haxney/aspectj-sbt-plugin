e8ce0c0488b4e731dc29920e942c0aa4ed667dcd
Dan Hackney <dan@haxney.org>
2011-03-23 22:05:35 -0400

    Remove extraneous code.
    All that is really needed is adding to the classloader, so a duplicate compiler
    config is not needed.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- src/main/scala/org/haxney/aspectj/sbt/Aspectj.scala
+++ src/main/scala/org/haxney/aspectj/sbt/Aspectj.scala
@@ -8,49 +8,17 @@ import org.aspectj.bridge.MessageHandler
 
 trait AspectJ extends BasicScalaProject with FileTasks with MavenStyleScalaPaths {
   lazy val aspectjTools = "org.aspectj" % "aspectjtools" % "1.6.11"
-  lazy val aspectjRt = "org.aspectj"    % "aspectjrt"    % "1.6.11"
   lazy val aspectjConf = config("aspectj")
 
   implicit def sting2CompileOption(opts: Iterable[String]) = opts.map(CompileOption.apply)
   implicit def sting2JavaCompileOption(opts: Iterable[String]) = opts.map(JavaCompileOption.apply)
 
   def aspectjPaths: PathFinder = configurationClasspath(aspectjConf)
-  def aspectjArgs = List("-1.5")
-
-  /* use default compile options */
-  def aspectjCompileOptions: Seq[JavaCompileOption] = javaCompileOptions ++ aspectjArgs
-  def aspectjLabel = "aspectj"
-  def aspectjSourcePath = mainJavaSourcePath
-  def aspectjSourceRoots = (aspectjSourcePath ##)
-  def aspectjSources = sources(aspectjSourceRoots)
-  def aspectjCompilePath = mainCompilePath
-  def aspectjAnalysisPath = mainAnalysisPath
-  def aspectjClasspath = compileClasspath +++ aspectjPaths
-  def aspectjCompileConfiguration = new AspectjCompileConfig
-  def aspectjExtraJars = buildScalaInstance.extraJars ++ aspectjPaths.getFiles
   def aspectjCompilerJar = aspectjPaths.getFiles.find(_.getName.contains("aspectj-compiler-facade")).get
-  def aspectjScalaInstance = new ScalaInstance(buildScalaInstance.version,
-                                               buildScalaInstance.loader,
-                                               buildScalaInstance.libraryJar,
-                                               aspectjCompilerJar,
-                                               aspectjExtraJars)
-  def aspectjBuildCompiler = new AnalyzingCompiler(aspectjScalaInstance, componentManager, log)
-  def aspectjCompileConditional = new CompileConditional(aspectjCompileConfiguration, aspectjBuildCompiler)
-  def aspectjCompileDescription = "Compiles Java sources with AspectJ"
 
-  class AspectjCompileConfig extends BaseCompileConfig {
-    def baseCompileOptions = compileOptions
-    def label = aspectjLabel
-    def sourceRoots = aspectjSourceRoots
-    def sources = aspectjSources
-    def outputDirectory = aspectjCompilePath
-    def classpath = aspectjClasspath
-    def analysisPath = aspectjAnalysisPath
-    def fingerprints = Fingerprints(Nil, Nil)
-    def javaOptions = ScalaProject.javaOptionsAsString(aspectjCompileOptions)
-  }
+  override def javaCompileOptions = List("-1.5", "-aspectpath", compileClasspath.absString).map(JavaCompileOption.apply) ++ super.javaCompileOptions
 
-  protected def aspectjAction = task {
+  protected def aspectjSetup = {
     import java.net.{URLClassLoader, URL}
     val loader = classOf[Compile].getClassLoader.asInstanceOf[URLClassLoader]
     val addURLMethod = loader.getClass.getDeclaredMethod("addURL", classOf[URL])
@@ -60,10 +28,12 @@ trait AspectJ extends BasicScalaProject with FileTasks with MavenStyleScalaPaths
     def toURL(f: java.io.File) = f.toURL
 
     (aspectjPaths.getFiles + buildScalaInstance.libraryJar) map toURL foreach addURL
+  }
 
-    aspectjCompileConditional.run
-  } describedAs aspectjCompileDescription
+  override def compileAction = {
+    aspectjSetup
+    super.compileAction
+  }
 
-  lazy val aspectj = aspectjAction
   override def compileOrder = CompileOrder.JavaThenScala
 }
