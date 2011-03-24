37c93d2db428e46b51254a891ce109ae4a8be1ee
Dan Hackney <dan@haxney.org>
2011-03-24 02:03:46 -0400

    Move back to external compiler facade.
    It needs to be external since we need to be able to use the facade jar when
    building the compiler arguments. If it is only included in a plugin, it's hard
    to get the path to the class files.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- src/main/scala/org/haxney/aspectj/sbt/Aspectj.scala
+++ src/main/scala/org/haxney/aspectj/sbt/Aspectj.scala
@@ -7,14 +7,13 @@ import org.aspectj.bridge.IMessage
 import org.aspectj.bridge.MessageHandler
 
 trait AspectJ extends BasicScalaProject with FileTasks with MavenStyleScalaPaths {
-  lazy val aspectjTools = "org.aspectj" % "aspectjtools" % "1.6.11"
+  lazy val facade = "org.haxney.aspectj" %% "aspectj-compiler-facade" % "0.1" % "aspectj"
   lazy val aspectjConf = config("aspectj")
 
   implicit def sting2CompileOption(opts: Iterable[String]) = opts.map(CompileOption.apply)
   implicit def sting2JavaCompileOption(opts: Iterable[String]) = opts.map(JavaCompileOption.apply)
 
   def aspectjPaths: PathFinder = configurationClasspath(aspectjConf)
-  def aspectjCompilerJar = aspectjPaths.getFiles.find(_.getName.contains("aspectj-compiler-facade")).get
 
   override def javaCompileOptions = List("-1.5", "-aspectpath", compileClasspath.absString).map(JavaCompileOption.apply) ++ super.javaCompileOptions
 
